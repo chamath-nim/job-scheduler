8af94e6446c1d22897cea10179225c83a212e19e
chamath-nim <pkchama2001@gmail.com>
2022-11-08 11:07:55 +0530

    new update

---
+++ SR/src/main/java/com/mobitel/servicereq/model/MainJobs.java
@@ -0,0 +1,12 @@
+package com.mobitel.servicereq.model;
+
+import lombok.Data;
+
+@Data
+public class MainJobs {
+
+    String tiggerName;
+    String triggerGroup;
+    String triggerState;
+    String description;
+}
