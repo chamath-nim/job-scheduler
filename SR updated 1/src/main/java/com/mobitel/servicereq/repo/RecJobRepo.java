a404769c1f69c5bcf3d64e49bf6170953e9e81c0
chamath-nim <pkchama2001@gmail.com>
2022-11-08 11:02:07 +0530

    new update

--- SR updated 1/src/main/java/com/mobitel/servicereq/repo/RecJobRepo.java
+++
@@ -1,7 +0,0 @@
-package com.mobitel.servicereq.repo;
-
-import com.mobitel.servicereq.model.RecJobDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecJobRepo extends JpaRepository<RecJobDetails, Long> {
-}
